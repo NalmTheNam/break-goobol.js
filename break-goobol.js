391489c71315a528848f43806c282fddb7a1a8a6
Glitch (pear-coconut-pyjama) <none>
2024-11-24 14:15:34 +0000

    🐣🎭 Checkpoint ./break-goobol.js:20101391/652

--- break-goobol.js
+++ break-goobol.js
@@ -1,9 +1,27 @@
 class BAN {
   constructor(numberOrArray) {
-    if (numberOrArray instanceof Array) 
-      return this.numberArray = numberOrArray
+    if (numberOrArray instanceof Array) {
+      this.numberArray = numberOrArray
+      
+      // Rule 2 of BAN: If the last entry is 1, it can be removed
+      
+      /*
+      const entryCount = this.numberArray.length - 1
+      
+      const lastEntry = this.numberArray[entryCount]
+      if (lastEntry === 1) this.numberArray.pop()
+      */
+      
+      return
+    }
     
     const number = numberOrArray
     this.numberArray = [number]
+  }
+  
+  toString() {
+    const entryCount = this.numberArray.length
+    
+    this.numberArray
   }
 }
