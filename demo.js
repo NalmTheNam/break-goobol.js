d14324576480965599eb6800fd845f0118d15d7d
Glitch (pear-coconut-pyjama) <none>
2025-01-01 13:27:23 +0000

    🚄👨‍❤️‍💋‍👨 Checkpoint ./demo.js:20101391/2300

--- demo.js
+++ demo.js
@@ -7,26 +7,53 @@ html`
     <h1>break-goobol.js</h1>
     <h2 class="number">${number}</h2>
     <p class="plain-notation-number">Plain number: 0</p>
-    ${NumberControlPanel}
+    ${NumberControlPanel()}
 `.render(document.getElementById("app"))
 
 function NumberControlPanel() {
-  const addNumber = () => {
-    updateNumber(num => num.add(1))
+  const controlButtons = {
+    "Add +1 to number": {
+      press: () => updateNumber(num => num.add(1))
+    },
+    "Add +10 to number": {
+      press: () => updateNumber(num => num.add(10))
+    },
+    "Add +100 to number": {
+      press: () => updateNumber(num => num.add(100))
+    },
+    "Add +1000 to number": {
+      press: () => updateNumber(num => num.add(1000))
+    },
+    "Add 10% to number": {
+      press: () => updateNumber(num => num.add(num.toNumber() / 10))
+    },
+    "Multiply number by 2": {
+      press: () => updateNumber(num => num.mul(2))
+    },
+    "Multiply number by 3": {
+      press: () => updateNumber(num => num.mul(3))
+    },
+    "Multiply number by 10": {
+      press: () => updateNumber(num => num.mul(10))
+    },
+    "Raise number to the second power": {
+      press: () => updateNumber(num => num.pow(2))
+    },
+    "Raise number to the third power": {
+      press: () => updateNumber(num => num.pow(3))
+    }
   }
   
-  const raiseNumber = () => {
-    updateNumber(num => num.pow(2))
-  }
-  
-  const raiseNumberTo10 = () => {
-    updateNumber(num => num.pow(10))
+  function renderPanelButtons() {
+    return html`
+      ${Object.entries(controlButtons).map(([name, button]) => {
+        return html`<button onclick=${button.press}>${name}</button>`
+      })}
+    `
   }
   
   return html`
-    <button onclick="${addNumber}">Add +1 to number</button>
-    <button onclick="${raiseNumber}">Raise the number to the power of 2</button>
-    <button onclick="${raiseNumberTo10}">Raise the number to the power of 10</button>
+    ${renderPanelButtons()}
   `
 }
 
