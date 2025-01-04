ee6ba2993485b00c12f7b7c7ca36fad968d5d290
Glitch (pear-coconut-pyjama) <none>
2025-01-04 03:19:22 +0000

    ⛄️🏚 Checkpoint ./ban-intl.js:20101391/644

--- ban-intl.js
+++ ban-intl.js
@@ -1,7 +1,20 @@
+import { Decimal } from "./break-goobol.js"
+
 class BANFormat extends Intl.NumberFormat {
   format(value) {
     const options = this.resolvedOptions()
-    if (options.)
+    
+    if (options.notation == "compact") {
+      const illions = [{
+        before36OoM: ["K", "M", "B", "T", "Qa", "Qt", "Sx", "Sp", "Oc", "No", "Dc"],
+        before306OoM: ["Vg", "Tg", "Qd", "Qi", "Se", "St", "Og", "Nn", "Ce"]
+      }]
+      
+      const illionPrefixes = ["U", "D", "T", "Qa", "Qt", "Sx", "Sp", "O", "N"]
+      
+      
+    }
+    
     const formattedValue = super.format(value)
     return formattedValue
   }
