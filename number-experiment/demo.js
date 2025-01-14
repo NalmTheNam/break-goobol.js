04a30d43e8d31139a76de5a16f14961998d5b6b2
NalmTheNam <75718836+NalmTheNam@users.noreply.github.com>
2025-01-14 11:34:47 +0700

    Make number experiment control panel more accessible

--- number-experiment/demo.js
+++ number-experiment/demo.js
@@ -16,53 +16,54 @@ html`
 
 function numberControlPanel() {
   const controlButtons = {
-    "Add +1 to number": {
+    "+1": {
       press: () => updateNumber(num => num.add(1))
     },
-    "Add +10 to number": {
+    "+10": {
       press: () => updateNumber(num => num.add(10))
     },
-    "Add +100 to number": {
+    "+100": {
       press: () => updateNumber(num => num.add(100))
     },
-    "Add +1000 to number": {
+    "+1000": {
       press: () => updateNumber(num => num.add(1000))
     },
-    "Add 10% to number": {
+    "+10%": {
       press: () => updateNumber(num => num.add(num.mul(0.1)))
     },
-    "Multiply number by 2": {
+    "x2": {
       press: () => updateNumber(num => num.mul(2))
     },
-    "Multiply number by 3": {
+    "x3": {
       press: () => updateNumber(num => num.mul(3))
     },
-    "Multiply number by 10": {
+    "x10": {
       press: () => updateNumber(num => num.mul(10))
     },
-    "Raise number to the second power": {
+    "^2": {
       press: () => updateNumber(num => num.pow(2))
     },
-    "Raise number to the third power": {
+    "^3": {
       press: () => updateNumber(num => num.pow(3))
     },
-    "Reset number to 0": {
+    "AC": {
       press: () => updateNumber(num => new BAN(0))
     },
   }
   
   function renderPanelButtons() {
     return html`
-      <div class="control-panel-buttons">
-        ${Object.entries(controlButtons).map(([name, button]) => {
-          return html`<button onclick=${button.press}>${name}</button>`
-        })}
-      </div>
+      ${Object.entries(controlButtons).map(([name, button]) => {
+        return html`<button onclick=${button.press}>${name}</button>`
+      })}
     `
   }
   
   return html`
-    ${renderPanelButtons()}
+    <h2>Control Panel</h2>
+    <div class="control-panel-buttons">
+      ${renderPanelButtons()}
+    </div>
   `
 }
 
